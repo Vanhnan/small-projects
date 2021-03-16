33649e7dda58f9a082dab69262d955e993cc3e88
v4h4 <vanhan.muhtaru@gmail.com>
2021-03-16 10:22:26 +0600

    Delete first_simple_telegram_bot directory

--- first_simple_telegram_bot/config.py
+++
@@ -1,1 +0,0 @@
-token = "1390499791:AAH5CJ9HJLVmdDkthKlICYnrqM4_6mpTSII"
--- first_simple_telegram_bot/tttt.sql
+++
@@ -1,9 +0,0 @@
-BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "music" (
-	"Id"	INTEGER,
-	"file_id"	TEXT,
-	"right_asnwer"	TEXT,
-	"wrong_answer"	TEXT,
-	PRIMARY KEY("Id" AUTOINCREMENT)
-);
-COMMIT;
